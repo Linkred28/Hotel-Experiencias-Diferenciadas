e302889b156899bba4d648ac759d03abd804faa2
Linkred28 <areliaguilarln@gmail.com>
2025-09-14 03:46:45 -0600

    refactor: Rename project and update metadata
    Renames the project to "Commercial Strategy Slideshow" and updates related metadata, package name, and README. Also, makes minor styling adjustments to the timeline container and updates the SharePoint icon.

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect } from 'react';
 import { tools } from './constants';
 
--- types.ts
+++ types.ts
@@ -1,7 +1,7 @@
+
 import type React from 'react';
 
 export interface Tool {
   name: string;
-  description?: string;
   icon: React.ReactNode;
 }
